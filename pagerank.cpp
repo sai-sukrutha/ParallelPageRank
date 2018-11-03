825e915ba5656ebdfa4e12e60a8fa06401c7fa89
Sarvat <sarvat.ali@students.iiit.ac.in>
2018-11-03 14:09:15 +0530

    rank calculations of teams in Tournament and runnners participating in races

--- pagerank.cpp
+++ pagerank.cpp
@@ -2,10 +2,14 @@
 #include<vector>
 #include<array>
 #include<algorithm>
+#include <sstream>
+#include <string>
 using namespace std;
 
 vector<double> ranks;
 vector<double> Prevranks;
+vector<double> powerranks;
+vector<double> powerPrevranks;
 vector<int> outdegree;
 vector<vector<bool>> adj;
 
@@ -29,7 +33,22 @@ int j,count=0;
 	else
 		return false;
 }
+bool checksamepower()
+{
+int j,count=0;
+	
+		for(j=0;j<powerranks.size();j++)
+		{
 
+			if(abs(powerPrevranks[j]-powerranks[j])<0.0001)
+				count++;
+		}
+	
+	if(count==powerranks.size())
+		return true;
+	else
+		return false;
+}
 
 void calculateRank()
 {
@@ -58,32 +77,32 @@ if(checksame()||count==15)
 else
 	copy(ranks.begin(), ranks.end(), Prevranks.begin());
         
-for(int i=0;i<ranks.size();i++)
+/*for(int i=0;i<ranks.size();i++)
 {
 cout<<"  "<<ranks[i];
 
 }
-cout<<"\n";
+cout<<"\n";*/
 }
 
 }
 void mul(vector<vector<double>> a,vector<vector<double>> b)
 {
 
- vector<vector<double>> mul(ranks.size(),vector<double>(ranks.size(),0)); 
-    for (int i = 0; i < ranks.size(); i++) 
+ vector<vector<double>> mul(powerranks.size(),vector<double>(powerranks.size(),0)); 
+    for (int i = 0; i < powerranks.size(); i++) 
     { 
-        for (int j = 0; j < ranks.size(); j++) 
+        for (int j = 0; j < powerranks.size(); j++) 
         { 
             mul[i][j] = 0; 
-            for (int k = 0; k < ranks.size(); k++) 
+            for (int k = 0; k < powerranks.size(); k++) 
                 mul[i][j] += a[i][k]*b[k][j]; 
         } 
     } 
   
    
-    for (int i=0; i<ranks.size(); i++) 
-        for (int j=0; j<ranks.size(); j++) 
+    for (int i=0; i<powerranks.size(); i++) 
+        for (int j=0; j<powerranks.size(); j++) 
             powerMatrix[i][j] = mul[i][j];
 
 }
@@ -102,25 +121,25 @@ else
 }
 void mulwithrank(vector<vector<double>> adj,vector<double> v)
 {
-vector<double>mul(ranks.size(),0); 
-    for (int i = 0; i< ranks.size(); i++) 
+vector<double>mul(powerranks.size(),0); 
+    for (int i = 0; i< powerranks.size(); i++) 
     { 
         for (int j = 0; j < 1; j++) 
         { 
             mul[i]= 0; 
-            for (int k = 0; k < ranks.size(); k++) 
+            for (int k = 0; k < powerranks.size(); k++) 
                 mul[i] += adj[i][k]*v[k]; 
         } 
     } 
   
-   for(int i=0;i<ranks.size();i++)
-   	ranks[i]=mul[i];
+   for(int i=0;i<powerranks.size();i++)
+   	powerranks[i]=mul[i];
 
 
 }
 void calculateRankPower()
 {
-	rankinit.assign(ranks.begin(),ranks.end());
+	rankinit.assign(powerranks.begin(),powerranks.end());
 	
 	for(int i=1;i<100;i++)
         {
@@ -128,40 +147,46 @@ void calculateRankPower()
         	power(powerMatrix,i);
         	
 		mulwithrank(powerMatrix,rankinit);
-		if(checksame())
+		if(checksamepower())
 			break;
 		else
-			copy(ranks.begin(), ranks.end(), Prevranks.begin());
-		cout<<endl;
+			copy(powerranks.begin(), powerranks.end(), powerPrevranks.begin());
+		
+/*for(int i=0;i<prevranks.size();i++)
+{
+cout<<"  "<<prevranks[i];
 
-		for(int j=0;j<ranks.size();j++)
-                 {
-                         cout<<" power "<<ranks[j];
-
-			}
-		cout<<"\n";
+}
+cout<<"\n";*/
+		
 			}
 	
 }
 int main()
 {
+	int game;
+	cout<<"Press 1 for Tournament, 2 for Race ";
+    cin>>game;
   cout.precision(5);
-  int count=0;
+  int count=0,temp;
   int nplayers;
-  cout<<"Enter Number of players";
+  if(game==1)
+  cout<<"Enter Number of Teams ";
+ else
+ 	cout<<"Enter Number of participants ";
   cin>>nplayers;
   for(int i=0;i<nplayers;i++)
 {
 
 
 	ranks.push_back(1.0/nplayers);
+	powerranks.push_back(1.0/nplayers);
 }
 Prevranks.resize(nplayers);
+powerPrevranks.resize(nplayers);
 copy(ranks.begin(), ranks.end(), Prevranks.begin());
-for(int i=0;i<nplayers;i++)
-{
-cout<<" "<<ranks[i]<<"\n";
-}
+copy(powerranks.begin(), powerranks.end(), powerPrevranks.begin());
+
 int m,n,matches;
 outdegree.resize(nplayers);
 adj.resize(nplayers);
@@ -173,20 +198,54 @@ for (int i = 0; i < nplayers; ++i)
 initialMatrix.resize(nplayers);
 for (int i = 0; i < nplayers; ++i)
     initialMatrix[i].resize(nplayers);
-
-  while(count++ <1)
-  {
-  	cout<<"enter matches";
+if(game==1)
+cout<<"Enter Number of Tournament ";
+else
+cout<<"Enter Number of Races ";
+cin>>temp;
+  while(count++ <temp)
+  {     
+  	if(game==1)
+  	{
+  	cout<<"Enter Number of matches for Tournament "+to_string(count)<<endl;
   	cin>>matches;
   	while(matches-- >0)
   	{
-                cout<<"Enter winner and loser";
+                cout<<"Enter winner and loser ";
   		cin>>m>>n;
   		
 outdegree[n]++;
 adj[m][n]=true;
 }
+}
+else
+{
+	
+	cout<<"Enter Race Completion order for Race "+to_string(count)<<endl;
+	
+vector<int> r;
+int x;
 
+while( cin>>x )
+{
+	r.push_back(x);
+	if(cin.get()=='\n')
+		break;
+}
+
+int i=0,j=0;
+for(i=0;i<r.size()-1;i++)
+{
+	for(j=i+1;j<r.size();j++)
+	{
+		adj[r[i]][r[j]]=true;
+		outdegree[r[j]]++;
+	}
+}
+
+
+
+}
 for(int i=0;i<nplayers;i++)
 {
 	for(int j=0;j<nplayers;j++)
@@ -205,37 +264,40 @@ for(int i=0;i<nplayers;i++)
 initialMatrix.assign(powerMatrix.begin(),powerMatrix.end());
 
 
-for(int i=0;i<nplayers;i++)
-{
-	for(int j=0;j<nplayers;j++)
-	{
-		cout<<fixed<<powerMatrix[i][j];
-	}
-	cout<<"\n";
-}
 
 
+cout.precision(5);
 calculateRank();
+
+cout<<"Vanilla Page Ranks "<<endl;
 for(int i=0;i<nplayers;i++)
 {
 cout<<"  "<<ranks[i];
 }
 
 cout<<"\n";
-cout<<*max_element(ranks.begin(),ranks.end());
+cout<<"Max-Rank Vanilla  "<<*max_element(ranks.begin(),ranks.end())<<endl;
+
+
+
 cout.precision(30);
-for(int i=0;i<nplayers;i++)
-{
-ranks[i]=1.0/nplayers;
-}
 
 
-copy(ranks.begin(), ranks.end(), Prevranks.begin());
+
+
 
 calculateRankPower();
-cout<<endl;
-cout<<"max rank"<<*max_element(Prevranks.begin(),Prevranks.end());
+cout<<"Power matrix Page Ranks  "<<endl;
+for(int j=0;j<powerranks.size();j++)
+                 {
+                         cout<<" "<<powerranks[j];
 
+			}
+		
+cout<<endl;
+cout<<"Max-Rank Power matrix  "<<*max_element(powerranks.begin(),powerranks.end())<<endl;
 }
+
+
 	return 0;
 }
